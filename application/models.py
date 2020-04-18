ab97d39cc0003349b5ba3122a372782c48d05545
Todd Birchard <toddbirchard@gmail.com>
2020-04-18 02:57:24 -0400

    Simplified config.

--- application/models.py
+++ application/models.py
@@ -8,7 +8,6 @@ class User(UserMixin, db.Model):
     """Model for user accounts."""
 
     __tablename__ = 'flasklogin-users'
-
     id = db.Column(db.Integer,
                    primary_key=True)
     name = db.Column(db.String,
--- config.py
+++ config.py
@@ -1,35 +1,34 @@
 """App configuration."""
 from os import environ, path
-import redis
-
 from dotenv import load_dotenv
+import redis
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
-    """Set Flask configuration vars from .env file."""
+    """Set Flask configuration variables from .env file."""
 
     # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
 
     # Flask-Session
-    SESSION_TYPE = environ.get('SESSION_TYPE')
+    SESSION_TYPE = 'redis'
     SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
 
     # Flask-Assets
     LESS_BIN = environ.get('LESS_BIN')
-    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
-    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+    ASSETS_DEBUG = False
+    LESS_RUN_IN_DEBUG = False
 
     # Static Assets
-    STATIC_FOLDER = environ.get('STATIC_FOLDER')
-    TEMPLATES_FOLDER = environ.get('TEMPLATES_FOLDER')
-    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = True
 
     # Flask-SQLAlchemy
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
