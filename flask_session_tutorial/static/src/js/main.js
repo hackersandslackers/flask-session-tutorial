51923fd0a001b05a12ae80635cdb5183a223eccc
Todd Birchard <toddbirchard@gmail.com>
2023-12-13 08:33:26 -0500

    Stylesheet refactor.

--- flask_session_tutorial/static/src/js/main.js
+++ flask_session_tutorial/static/src/js/main.js
@@ -1,14 +1,16 @@
 // Remove Alert on Close
-let alertButton = document.querySelector('.alert button');
+let alertButton = document.querySelector(".alert button");
 
-if (alertButton){
-  alertButton.addEventListener('click', function (event) {
+if (alertButton) {
+  alertButton.addEventListener(
+    "click",
+    function (event) {
+      // If the clicked element doesn't have the right selector, bail
+      alertButton.parentNode.style.display = "none";
 
-  	// If the clicked element doesn't have the right selector, bail
-    alertbutton.parentNode.style.display = 'none';
-
-  	// Don't follow the link
-  	event.preventDefault();
-
-  }, false);
+      // Don't follow the link
+      event.preventDefault();
+    },
+    false
+  );
 }
